a5263613877416fac487ec2d04d94548afbcf850
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-17 17:24:20 -0300

    Atualiza script para rodar todos os casos em uma unica execucao

--- etapa1/benchmark.sh
+++ etapa1/benchmark.sh
@@ -2,21 +2,29 @@
 
 #constants
 MAX_ITERATIONS=10
-FILE_NAME="output.txt"
+FILE_NAME_START="bmk_"
+FILE_EXTENSION=".txt"
+QTT_GOROUTINES=(0001 0005 0010 0050 0100 0500 1000 5000)
 
-#script
-touch $FILE_NAME #guarantees that file exists
-rm $FILE_NAME #so it doesnt print unnecessary info
 
-#runs the program the desired number of times
-for i in `seq 1 $MAX_ITERATIONS`;
+for i in "${QTT_GOROUTINES[@]}";
 do
-	start=$( date +"%T" )
-	go run etapa1.go
-	end=$( date +"%T" )
-	echo "-------------------------ITERATION #"$i"-------------------------" >> $FILE_NAME
-	echo "Start time: "$start >> $FILE_NAME
-	echo "Finished time: "$end >> $FILE_NAME
-	echo "Duration: "$(($(date -d $end +%s) - $(date -d $start +%s))) "second(s)" >> $FILE_NAME
-	echo"" >> $FILE_NAME
+	FILE=$FILE_NAME_START$i$FILE_EXTENSION
+	#script
+	touch $FILE #guarantees that file exists
+	rm $FILE #so it doesnt print unnecessary info
+	echo $FILE
+
+	#runs the program the desired number of times
+	for j in `seq 1 $MAX_ITERATIONS`;
+	do
+		start=$( date +"%T" )
+		go run etapa1.go 5000 $i
+		end=$( date +"%T" )
+		echo "-------------------------ITERATION #"$j"-------------------------" >> $FILE
+		echo "Start time: "$start >> $FILE
+		echo "Finished time: "$end >> $FILE
+		echo "Duration: "$(($(date -d $end +%s) - $(date -d $start +%s))) "second(s)" >> $FILE
+		echo"" >> $FILE
+	done
 done
