797d9ee62e6c3e7b6ee0ef079f6b1af8b9cea856
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-17 12:32:55 -0300

    Cria prototipo de consumidor

--- etapa1.go
+++ etapa1.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 )
 
+const BUFFER_SIZE = 10
+
+//estrutura que representa um pedido
 type Pedido struct {
-	id int
+	id int //identificador
 	dados string
+}
+
+/*gorotina consumidora que consumira um canal
+bufferizado com 5000 pedidos*/
+func consumidor (ch chan Pedido) {
+	for p := range ch {
+		fmt.Println(p)
+	}
 }
 
 func main() {
 	var p Pedido
-	p.id = 123
-	p.dados = "ai dento"
-	fmt.Println(p)
+	ch := make(chan Pedido, BUFFER_SIZE) //cria canal
+
+	for i := 1; i <= BUFFER_SIZE; i++ {
+		p = Pedido{i, "Dados do pedido #" + strconv.Itoa(i)}
+		ch <- p
+	}
+	close(ch)
+	go consumidor(ch)
+	time.Sleep(1)
+	
 }
