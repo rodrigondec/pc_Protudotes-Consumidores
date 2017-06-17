bdb0bc67ed9b2aa1fb3a0f08a77e1dff766f2fdb
Gustavo <gustavoalvesbezerra@gmail.com>
2017-06-17 13:40:18 -0300

    Finaliza implementacao da etapa 1 - (a ser verificado)

--- etapa1.go
+++ etapa1.go
@@ -6,7 +6,8 @@ import (
 	"time"
 )
 
-const BUFFER_SIZE = 10
+const TAMANHO_BUFFER = 5000
+const QTD_CONSUMIDORES = 100
 
 //estrutura que representa um pedido
 type Pedido struct {
@@ -16,22 +17,33 @@ type Pedido struct {
 
 /*gorotina consumidora que consumira um canal
 bufferizado com 5000 pedidos*/
-func consumidor (ch chan Pedido) {
+func consumidor (ch chan Pedido, n int) {
 	for p := range ch {
-		fmt.Println(p)
+		fmt.Println("{" + strconv.Itoa(p.id) + ", " + p.dados +
+			"} GoRotina: " + strconv.Itoa(n))
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
 func main() {
 	var p Pedido
-	ch := make(chan Pedido, BUFFER_SIZE) //cria canal
+	ch := make(chan Pedido, TAMANHO_BUFFER) //cria canal
 
-	for i := 1; i <= BUFFER_SIZE; i++ {
+	//loop adiciona pedidos no canal
+	for i := 1; i <= TAMANHO_BUFFER; i++ {
 		p = Pedido{i, "Dados do pedido #" + strconv.Itoa(i)}
 		ch <- p
 	}
-	close(ch)
-	go consumidor(ch)
-	time.Sleep(1)
+	close(ch) //fecha o canal
+
+	for i := 0; i < QTD_CONSUMIDORES; i++ {
+		go consumidor(ch, i)
+	}
+
+	//espera termino de execucao dos consumidores iterativamente
+	for ; len(ch) > 0; {
+		fmt.Println("OI " + strconv.Itoa(len(ch)))
+		time.Sleep(500 * time.Millisecond);
+	}
 	
 }
