f28d456ebc0cdbd38c9406cde73db25d6f29ba45
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-21 15:47:48 -0300

    etapa 2 com mutex

--- etapa2/etapa2.go
+++ etapa2/etapa2.go
@@ -9,10 +9,16 @@ import (
 )
 
 const SEPARADOR = " / "
-const TAMANHO_BUFFER = 5000
-var id_pedido = 1
+const TAMANHO_BUFFER = 10
+
+//var id_pedido = 1
 var pedidos_terminados = false
 var is_channel_closed = false
+
+var id_pedido struct{
+	sync.Mutex
+	n int
+}
 
 //estrutura que representa um pedido
 type Pedido struct {
@@ -24,8 +30,9 @@ var wg sync.WaitGroup //cria grupo de espera
 
 /* gorotina consumidora que consumirá de um canal
 bufferizado com 5000 pedidos */
-func consumidor (ch chan Pedido, n int) {
+func consumidor (ch chan Pedido, n int, mutex *sync.Mutex) {
 	for p := range ch {
+		mutex.Lock()
 		horario_inicio := time.Now()
 		time.Sleep(500 * time.Millisecond)
 		horario_termino := time.Now()
@@ -35,6 +42,7 @@ func consumidor (ch chan Pedido, n int) {
 			"Inicio proc: " + horario_inicio.String() + SEPARADOR +
 			"Termino proc: " + horario_termino.String() + SEPARADOR +
 			"Duracao: " + horario_termino.Sub(horario_inicio).String())
+		mutex.Unlock()
 	}
 	wg.Done()
 }
@@ -54,16 +62,17 @@ func produtor (ch chan Pedido, n int, mutex_qt *sync.Mutex, mutex_id *sync.Mutex
 		var p Pedido
 		horario_inicio := time.Now()
 		time.Sleep(500 * time.Millisecond)
-		
-		if id_pedido > TAMANHO_BUFFER {
+		if id_pedido.n > TAMANHO_BUFFER {
 			if !is_channel_closed {
 				is_channel_closed = true
 				close(ch)
 			}
 			return
 		}
-		p = Pedido{id_pedido, "Dados do pedido #" + strconv.Itoa(id_pedido)}
-		id_pedido += 1
+		id_pedido.Lock()
+		p = Pedido{id_pedido.n, "Dados do pedido #" + strconv.Itoa(id_pedido.n)}
+		id_pedido.n += 1
+		id_pedido.Unlock()
 		ch <- p
 		horario_termino := time.Now()
 
@@ -82,9 +91,11 @@ func main() {
 	if len(os.Args) == 3 {
 		QTD_CONSUMIDORES, _ := strconv.Atoi(os.Args[1])
 		QTD_PRODUTORES, _ := strconv.Atoi(os.Args[2])
+		id_pedido.n = 1
 		ch := make(chan Pedido, TAMANHO_BUFFER) //cria canal
 		var m_qt_atividades = &sync.Mutex{}
 		var m_id_atividades = &sync.Mutex{}
+		var m_print_con = &sync.Mutex{}
 
 		//executa todos os produtores
 		for i := 1; i <= QTD_PRODUTORES; i++ {
@@ -94,7 +105,7 @@ func main() {
 		//executa todos os consumidores
 		for i := 1; i <= QTD_CONSUMIDORES; i++ {
 			wg.Add(1)
-			go consumidor(ch, i)
+			go consumidor(ch, i, m_print_con)
 		}
 
 		//espera termino de execucao de todos os consumidores
